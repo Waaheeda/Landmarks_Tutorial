27de37f38f93a0fb7b8f95999b7d93731be93e9c
waaheeda <vahida.havaldar@gmail.com>
2022-11-11 16:33:54 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Vahida on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
